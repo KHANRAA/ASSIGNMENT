0bfbdf6d6a640a4a8c254b2839186c8f3841b537
KHANRAA <khanraakash@gmail.com>
2018-04-14 14:07:05 +0530

    Create incall.h

---
+++ LPU/incall.h
@@ -0,0 +1,14 @@
+//
+// Created by AKASH on 24-03-2018.
+//
+
+#ifndef ASSIGNMENT_INCALL_H
+#define ASSIGNMENT_INCALL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+#endif //ASSIGNMENT_INCALL_H
