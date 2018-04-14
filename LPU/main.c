b19fefa2ab2a79e39cf643a9e9c351b23335b9dd
KHANRAA <khanraakash@gmail.com>
2018-04-14 18:04:55 +0530

    major sortung fix

--- LPU/main.c
+++ LPU/main.c
@@ -17,59 +17,46 @@ void bubbleSort(int arr[], int m)
                 swap(&arr[j], &arr[j+1]);
 }
 
-/* Function to print an array
-void printArray(int arr[], int size)
-{
-    int i;
-    for (i=0; i < size; i++)
-        printf("%d ", arr[i]);
-    printf("n");
-}
-
- Driver program to test above functions
-int main()
-{
-    int arr[] = {64, 34, 25, 12, 22, 11, 90};
-    int n = sizeof(arr)/sizeof(arr[0]);
-    bubbleSort(arr, n);
-    printf("Sorted array: \n");
-    printArray(arr, n);
-    return 0;
-}
-
-
-*/
-
 
 int main()
 {
     int n;
     printf("Enter the no of process: \t");
     scanf("%d",&n);
-    int id[n],arr_time[n],bur_time[n];
-    clrscr();
+    int id[n],arr_time[n],bur_time[n],check[n],serial[n];;
+
+    //clrscr();
     for(int i=0;i<n;i++)
     {
         printf("Enter the process id: \t");
         scanf("%d",&id[i]);
         printf("\nEnter the Arrival time: \t");
-        scanf("%d",&arr_time[n]);
-        printf("\nEnter the Arrival time: \t");
-        scanf("%d",&bur_time[n]);
+        scanf("%d",&arr_time[i]);
+        printf("\nEnter the Burst time: \t");
+        scanf("%d",&bur_time[i]);
     }
-    int m = sizeof(arr_time)/sizeof(arr_time[0]);
-    bubbleSort(arr_time, m);
-    printf("Sorted array: \n");
-//printArray(arr, n);
-    clrscr();
-    printf("Process ID\tArrival Time\tBurst Time\n");
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++) {
+        check[i] = arr_time[i];
+    }
+    bubbleSort(arr_time, n);
+    for(int i=0;i<n;)
     {
-        print(" %d    \t    %d    \t    %d\n",id[i],arr_time[i],bur_time[i]);
-
+        for(int j=0;j<n;) {
+            if (check[i] == arr_time[j]) {
+                serial[i] = j;
+                i++;
+            }
+            else
+            j++;
+        }
     }
 
+    printf("Process id\tArrival time\tBurst time\n");
+    for(int i=0;i<n;i++) {
+        int b=serial[i];
+        printf(" %d    \t      %d    \t         %d\n",id[b],arr_time[i],bur_time[b]);
 
+    }
 
 
 
