922e2ce620032b9c31bb40febe9d085492cb4455
KHANRAA <khanraakash@gmail.com>
2018-04-14 15:48:33 +0530

    Update main.c

--- LPU/main.c
+++ LPU/main.c
@@ -1,5 +1,16 @@
 int main()
 {
+int n;
+  printf("Enter the no of process: \t");
+  scanf("%d",&n);
+  int id[n],arr_time[n],bur_time[n];
+  clrscr();
+  for(int i=0;i<n;i++)
+  {
+    printf("Enter the process id: \t");
+    scanf("%d",&id[i]);
+    
+  }
+  
 
-printf("Hello");
 }
